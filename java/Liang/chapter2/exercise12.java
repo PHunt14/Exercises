90b8d7b46750bffeabf6f333171583b5c927b9b7
PHunt14 <patrick@fortinbras.net>
2018-05-24 13:32:24 +0000

    started exercise12

---
+++ java/Liang/chapter2/exercise12.java
@@ -0,0 +1,23 @@
+/**
+ */
+
+package chapter2;
+
+import java.util.Scanner;
+
+public class exercise12 {
+	
+	// main method
+	public static void main(String[] args) {
+		// start scanner
+		Scanner input = new Scanner(System.in);
+		
+
+		// display results
+		System.out.println("The population will be " + firstYear + " after 1 year, " + secondYear +
+			" after 2 years, " + thirdYear + " after 3 years, " + fourthYear +
+			" after 4 years, " + fifthYear + " after 5 years." );
+
+	}
+
+}
