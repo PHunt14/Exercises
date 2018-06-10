8294477839ad5ca528d282e583c02dfd6392d782
PHunt14 <patrick@fortinbras.net>
2018-06-10 00:45:50 +0000

    started exercise 2.18

---
+++ java/Liang/chapter2/exercise18.java
@@ -0,0 +1,25 @@
+/** Write a program that displays the following table
+ *
+ * 	a		b		pow(a, b)
+ * 	1		2		1
+ * 	2		3		8
+ * 	3		4		81
+ * 	4		5		1024
+ * 	5		6		15625
+ */
+
+package chapter2;
+
+import java.util.Scanner;
+
+public class exercise18 {
+	
+	// main method
+	public static void main(String[] args) {
+		
+		// display results
+		System.out.println("The wind chill index is " + windChill);
+
+	}
+
+}
